21f43d80058e0b601da15d441bc69ea0ec894035
Yadiel-Suarez <yadiel.suarez1@upr.edu>
2022-08-31 19:14:35 -0400

    Added method functions to Triangle.h

--- heronTriangle.cpp
+++ heronTriangle.cpp
@@ -4,35 +4,15 @@
 
 using namespace std;
 
-double getArea(Triangle t){
-    double s = (t.side1 + t.side2 + t.side3)/2.0;
-    double area = sqrt(s * (s-t.side1) * (s-t.side2) * (s-t.side3));
-    return area;
-}
-
-bool hasLargerArea(Triangle t1, Triangle t2){
-    return (getArea(t1) > getArea(t2));
-}
-
-double sumOfAreas(Triangle triangles[], int size){
-    double result = 0;
-    for(int i=0; i < size; i++)
-        result += getArea(triangles[i]);
-    return result;
-}
-
-Triangle scale(Triangle original, double scale){
-    return Triangle(original.side1*scale, original.side2*scale, original.side3*scale);
-}
 
 
 int main(){
-    double areaResult = getArea(Triangle(10,10,10));
+    double areaResult = Triangle(10,10,10).getArea();
     cout << "Area of triangle is: " << areaResult << endl;
 
-    areaResult = getArea(Triangle(20,20,30));
+    areaResult = Triangle(20,20,30).getArea();
     cout << "Area of triangle is: " << areaResult << endl;
 
-    Triangle result = scale(Triangle(10,10,10), 2);
+    Triangle result = Triangle(10,10,10).scale(2);
 
 }
--- triangle.h
+++ triangle.h
@@ -14,4 +14,29 @@ class Triangle{
             this->side2 = s2;
             this->side3 = s3;
         }
+
+        double getArea(){
+            double s = (this->side1 + this->side2 + this->side3)/2.0;
+            double area = sqrt(s * (s-this->side1) * (s-this->side2) * (s-this->side3));
+            return area;
+        }
+        bool hasLargerArea(Triangle t2){
+            return (this->getArea() > t2.getArea());
+        }
+        double sumOfAreas(Triangle triangles[], int size){
+            double result = 0;
+            for (int i = 0; i < size; i++)
+                result += triangles[i].getArea();
+            return result;
+        }
+        Triangle scale(double scale){
+            return Triangle(this->side1 * scale, this->side2 * scale, this->side3 * scale);
+        }
 };
+
+
+
+
+Triangle scale(Triangle original, double scale){
+    return Triangle(original.side1*scale, original.side2*scale, original.side3*scale);
+}
