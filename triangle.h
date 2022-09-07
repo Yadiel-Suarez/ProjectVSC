1e6f30f93a0d1ac1ecd8220dba61ef2386291899
Yadiel-Suarez <yadiel.suarez1@upr.edu>
2022-09-07 18:28:28 -0400

    Added setters

--- triangle.h
+++ triangle.h
@@ -10,9 +10,15 @@ class Triangle{
         int side3;
     
     public:
-        int getSide1(){return side1;}
-        int getSide2(){return side2;}
-        int getSide3(){return side3;}
+        //Getters
+        int getSide1() {return side1;}
+        int getSide2() {return side2;}
+        int getSide3() {return side3;}
+
+        //Setters
+        void setSide1(int side1) {this->side1 = side1;}
+        void setSide2(int side2) {this->side2 = side2;}
+        void setSide3(int side3) {this->side3 = side3;}
 
         Triangle(int s1, int s2, int s3){
             this->side1 = s1;
